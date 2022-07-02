934d7887e994c7f53e348a122146ea9ea9b4cb01
Maruthi Akula <maruthiraoakula22@gmail.com>
2022-07-02 18:11:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parse_and_Tryparse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            string number = "1000";
+            int i = int.Parse(number);
+            Console.WriteLine(i);
+            string number_plus_characters = "1000bhehchd";
+            bool issuccessful= int.TryParse(number_plus_characters, out result);
+            if (issuccessful)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Enter valid number");
+            }
+        }
+    }
+}
